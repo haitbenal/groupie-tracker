326cc5ea7896faa37398fced24f6fbfeb297598a
yelhaoua <yacobelhaouarii@gmail.com>
2025-12-20 08:33:19 +0100

    Fixing the handlers and adding some style

--- main.go
+++ main.go
@@ -1,15 +1,29 @@
 package main
 
 import (
+	"fmt"
 	"groupie/handlers"
 	"net/http"
+	"strconv"
+	"strings"
 )
 
 func main() {
-	
-	http.HandleFunc("/statics/" , handlers.HandleForbiden)
-	http.HandleFunc("/", handlers.HandleHome)
-	err := http.ListenAndServe(":8080", nil)
+	mux := http.NewServeMux()
+	mux.HandleFunc("/statics/", handlers.HandleForbiden)
+	mux.HandleFunc("/", handlers.HandleHome)
+	mux.HandleFunc("/artist", func(w http.ResponseWriter, r *http.Request) {
+		path := r.URL.Path // مثال: /artist1
+		idStr := strings.TrimPrefix(path, "/artist")
+		id, err := strconv.Atoi(idStr)
+		if err != nil {
+			http.NotFound(w, r)
+			return
+		}
+		// هنا تستدعي HandlerInfo وتمرير الرقم
+		fmt.Println(id)
+	})
+	err := http.ListenAndServe(":8080", mux)
 	if err != nil {
 		// handlers.HandlerErr("Status Internal Server Error", http.StatusInternalServerError)
 	}
